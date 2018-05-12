f9dbc7f78d65c7f8a1fca52fdd93161508c910bb
tkrystkowiak <tokrystkowiak@gmail.com>
2018-05-12 20:04:49 +0200

    Created Second activity for adding hashtags

--- app/src/main/java/com/sdplab/twitterstat/twitterapp2/MainActivity.java
+++ app/src/main/java/com/sdplab/twitterstat/twitterapp2/MainActivity.java
@@ -1,13 +1,34 @@
 package com.sdplab.twitterstat.twitterapp2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button one = findViewById(R.id.button1);
+        one.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+
+            case R.id.button1:
+                StartAddHashActivity();
+                break;
+
+        }
+    }
+
+    private void StartAddHashActivity(){
+        Intent intent = new Intent(this, AddHashActivity.class);
+        startActivity(intent);
     }
 }
